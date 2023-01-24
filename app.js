cbfc5a985750f75bf0d41a124e4f5597edbdbeb1
rahmatdeep <rahmatdeep@gmail.com>
2023-01-25 03:24:47 +0530

    completed functionality

---
+++ app.js
@@ -0,0 +1,95 @@
+//DOM selectors
+const books = document.querySelector('.books-display');
+const container = document.querySelector('.container');
+const form = document.getElementById("form");
+const deleteButton = document.getElementById('deleteButton');
+const errorDisplay = document.querySelector('.error');
+const formSubmit = document.getElementById('formsubmit');
+
+
+//storage
+let booksInLibrary = [];
+
+//sample data
+let a = new bookTemplate('Silent Patient', 'Alex', 300)
+booksInLibrary.push(a);
+displayArray(booksInLibrary);
+//Functions
+
+function bookTemplate(title, author, pages){
+    this.title = title
+    this.author = author
+    this.pages = pages
+};
+
+
+const addBookToLibrary = function(event){
+    event.preventDefault();
+    let titleVal = document.getElementById('name').value
+    let authorVal = document.getElementById('author').value
+    let pagesVal = document.getElementById('pages').value
+
+    if(!(titleVal===''||authorVal===''||pagesVal==='')){
+    let nb = new bookTemplate(titleVal, authorVal, pagesVal);
+    booksInLibrary.push(nb);
+    formSubmit.setAttribute("data-bs-dismiss","modal")
+    formSubmit.click();
+    (()=>{
+        formSubmit.setAttribute("data-bs-dismiss","")
+    })()
+}
+    else{
+        errorDisplay.textContent='Form Cannot be Empty'
+    }
+    clearForm();
+    displayArray(booksInLibrary);
+    
+};
+
+function clearForm(){
+    document.getElementById('name').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pages').value = '';
+};
+
+function displayArray(Arr){  
+    container.innerHTML = ''
+    for(let i=0; i<Arr.length; i++){
+            let newBook = document.createElement('div');
+            newBook.classList.add('book');
+            newBook.dataset.index = i;
+            newBook.innerHTML = `<p><span>${Arr[i].title}</span> by <span>${Arr[i].author}</span> has <span>${Arr[i].pages}</span> pages </p><div class="right"><i onClick = "deleteElement(this)" class="fa-solid fa-trash"></i><i data-bs-toggle="modal" data-bs-target="#form" onClick="editElement(this)" class="fa-sharp fa-solid fa-pen-to-square"></i></div>`;
+            container.appendChild(newBook);
+        };
+};
+
+function deleteElement(e){
+    let selectedElement = e.parentElement.parentElement;
+    let toBeDeleted = selectedElement.firstChild.firstChild.textContent;
+    for (let i=0; i<booksInLibrary.length; i++){
+        if(toBeDeleted===booksInLibrary[i].title){
+            booksInLibrary.splice(i,1);
+        }
+    }
+    displayArray(booksInLibrary)
+}
+
+function editElement(e){
+    let selectedElement = e.parentElement.parentElement;
+    document.getElementById('name').value = selectedElement.firstChild.children[0].textContent;
+    document.getElementById('author').value = selectedElement.firstChild.children[1].textContent;
+    document.getElementById('pages').value = selectedElement.firstChild.children[2].textContent;
+    let toBeDeleted = selectedElement.firstChild.firstChild.textContent;
+    for (let i=0; i<booksInLibrary.length; i++){
+        if(toBeDeleted===booksInLibrary[i].title){
+            booksInLibrary.splice(i,1);
+        }
+    }
+    displayArray(booksInLibrary)
+}
+
+
+
+
+//Execution
+form.addEventListener('submit', addBookToLibrary);
